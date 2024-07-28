eef54266c7a09a6eae8b81b438ee75306e0b6655
nzlicrbc <nzlcn10@hotmail.com>
2024-07-28 22:58:20 +0300

    add SharedViewModel

--- app/src/main/java/com/example/task3a/ViewModel/SharedViewModel.kt
+++ app/src/main/java/com/example/task3a/ViewModel/SharedViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.task3a.ViewModel
 
-class SharedViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SharedViewModel : ViewModel() {
+    private val _hiddenNumber = MutableLiveData<Int>()
+    val hiddenNumber: LiveData<Int> = _hiddenNumber
+
+    fun setHiddenNumber(number: Int) {
+        _hiddenNumber.value = number
+    }
 }
