dc23ca573d63d86b255672258fe2e63366f18eb0
nzlicrbc <nzlcn10@hotmail.com>
2024-07-28 22:48:10 +0300

    make guess fragment and viewmodel

--- app/src/main/java/com/example/task3a/Fragment/GuessFragment.kt
+++ app/src/main/java/com/example/task3a/Fragment/GuessFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.task3a.ViewModel.GuessViewModel
 import com.example.task3a.databinding.FragmentGuessBinding
 
 class GuessFragment : Fragment() {
     private var _binding: FragmentGuessBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: GuessViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +20,59 @@ class GuessFragment : Fragment() {
     ): View {
         _binding = FragmentGuessBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupObservers()
+        setupClickListeners()
+    }
+
+    private fun setupObservers() {
+        viewModel.randomChar.observe(viewLifecycleOwner) { char ->
+            binding.textViewLetter.text = char.toString()
+        }
+
+        viewModel.result.observe(viewLifecycleOwner) { result ->
+            binding.textViewResult.text = result
+        }
+
+        viewModel.guessedNumber.observe(viewLifecycleOwner) { number ->
+            if(number != null) {
+                binding.textViewResult.text = number.toString()
+            } else {
+                binding.textViewResult.text = viewModel.result.value
+            }
+        }
+    }
+
+    private fun setupClickListeners() = with(binding) {
+        val numberButtons = listOf (
+            button0,
+            button1,
+            button2,
+            button3,
+            button4,
+            button5,
+            button6,
+            button7,
+            button8,
+            button9
+        )
+
+        numberButtons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                viewModel.selectNumber(index)
+            }
+        }
+
+        buttonClear.setOnClickListener {
+            viewModel.clearGuess()
+        }
+
+        buttonGuess.setOnClickListener {
+            viewModel.guess()
+        }
     }
 }
--- app/src/main/java/com/example/task3a/ViewModel/GuessViewModel.kt
+++ app/src/main/java/com/example/task3a/ViewModel/GuessViewModel.kt
@@ -1,4 +1,51 @@
 package com.example.task3a.ViewModel
 
-class GuessViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GuessViewModel : ViewModel() {
+    private val _randomNumber = MutableLiveData<Int>()
+    val randomNumber: LiveData<Int> = _randomNumber
+
+    private val _randomChar = MutableLiveData<Char>()
+    val randomChar: LiveData<Char> = _randomChar
+
+    private val _result = MutableLiveData<String>()
+    val result: LiveData<String> = _result
+
+    private val _guessedNumber = MutableLiveData<Int?>()
+    val guessedNumber: LiveData<Int?> = _guessedNumber
+
+    init {
+        generateNewNumberAndChar()
+    }
+
+    private fun generateNewNumberAndChar() {
+        _randomNumber.value = (0..9).random()
+        _randomChar.value = ('A'..'Z').random()
+        _result.value = "TEKRAR DENE"
+        _guessedNumber.value = null
+        _result.postValue(_result.value)
+    }
+
+    fun selectNumber(number: Int) {
+        _guessedNumber.value = number
+    }
+
+    fun guess() {
+        _guessedNumber.value?.let { guessedNumber ->
+            _randomNumber.value?.let { randomNumber ->
+                _result.value = if (guessedNumber == randomNumber) {
+                    "KAZANDIN!"
+                } else {
+                    "TEKRAR DENE"
+                }
+            }
+        }
+    }
+
+    fun clearGuess() {
+        generateNewNumberAndChar()
+    }
 }
